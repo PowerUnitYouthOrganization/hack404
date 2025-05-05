5c5de10cbe1dca0419a00e6081a866a8d649cfed
angelazhao787 <angelazhao787@gmail.com>
2025-05-04 21:10:53 -0400

    made new branch

---
+++ src/app/angelafile.js
@@ -0,0 +1,6 @@
+let today = new Date();
+console.log(today.toDateString()); // Outputs: "Sun May 4 2025"
+console.log (favouriteColor); // Outputs: "blue"
+let favouriteColor = "blue";
+console.log (favouriteColor); // Outputs: "blue"
+         

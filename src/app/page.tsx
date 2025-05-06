a57bc0297b6bd288f8b664aa83d830e5302b7ef9
gooosexe <olivershaohuang2006@gmail.com>
2025-05-06 01:47:37 -0400

    added github and google auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import SignInButton from "./components/signin";
+
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-[0px] row-start-2 items-center sm:items-start">
 				<img
 					src="FullBlackClear.png"
@@ -10,6 +12,10 @@ export default function Home() {
 				<p className="ml-20 text-2xl">
 					a toronto based hackathon, coming soon.
 				</p>
+				<div className="ml-20 mt-1 flex flex-row gap-5">
+					<SignInButton provider="github" />
+					<SignInButton provider="google" />
+				</div>
 			</main>
 		</div>
 	);

c23d15e83de9621ad5a6c9889f37aa9b85e27973
gooosexe <olivershaohuang2006@gmail.com>
2025-05-06 23:49:18 -0400

    formatted coming soon site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,36 @@
+import type { Metadata } from "next";
+import { Space_Grotesk, Space_Mono } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+	variable: "--font-space-grotesk",
+	subsets: ["latin"],
+});
+
+const spaceMono = Space_Mono({
+	weight: "400",
+	variable: "--font-space-mono",
+	subsets: ["latin"],
+});
+
 export const metadata = {
-    title: "Hack404",
-    description: "Hackathon landing page",
+	title: "Hack404",
+	description: "Hackathon landing page",
 };
 
 export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${spaceGrotesk.variable} ${spaceMono.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }

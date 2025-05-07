4053bbb702362e435403b090f2e307ae8b971818
gooosexe <olivershaohuang2006@gmail.com>
2025-05-07 02:09:48 -0400

    fixed colours, added email field opacity

--- src/app/components/h-border.tsx
+++ src/app/components/h-border.tsx
@@ -1,3 +1,3 @@
 export default function HBorder() {
-	return <div className="h-[1px] w-screen bg-white opacity-25"></div>;
+	return <div className="h-[1px] w-screen bg-[rgba(48,242,242,0.2)]" />;
 }

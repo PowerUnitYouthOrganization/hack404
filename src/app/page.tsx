f3e11e5739f2baf0a20f42a728cc6237e5484675
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-05-12 02:06:07 -0400

    should actually work now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,40 @@
 import Header from "./components/header";
 import { IoMdArrowForward } from "react-icons/io";
 import GradientBorder from "./components/gradient-border";
-import HBorder from "./components/h-border";
 import Grid from "./components/grid";
+import HBorder from "./components/h-border";
+import GradientBackground from "./components/gradient-background";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen flex flex-col bg-gradient-to-r from-[#4A32B0] via-[#20A0A0] to-[#8AA824] relative">
-      <Grid />
-			<div
-				className="absolute inset-0 -z-10 bg-[linear-gradient(white_0.5px,transparent_0.5px)] bg-[length:50px_50px] opacity-20"
-			></div>
+		<div className="min-h-screen flex flex-col relative">
 			<Header />
 			<HBorder />
-			<main className="flex-1 flex flex-col gap-8 items-center sm:items-start text-center sm:text-left">
-				<div className="flex-1 flex flex-col p-[64px] justify-between items-start">
-					<div className="flex justify-between items-start self-stretch">
-						<h1 className="text-white font-[300] text-[48px] leading-[110%] tracking-[-1.44px] font-(family-name:--font-heading-light)">
-							a toronto based <br /> hackathon
-						</h1>
-						<img
-							src="whitesmall.png"
-							alt=""
-							className="h-[80px] w-auto flex-shrink-0"
-						/>
-					</div>
+			<div className="flex-1 flex flex-col gap-8 items-start text-left p-[64px] justify-between text-white">
+				<div className="flex justify-between items-start self-stretch">
+					<h1 className="font-[300] text-[48px] leading-[110%] tracking-[-1.44px] font-(family-name:--font-heading-light)">
+						a toronto based <br /> hackathon
+					</h1>
 					<img
-						src="whitetext.png"
-						alt="hack404 big label"
-						className="h-auto w-full"
+						src="whitesmall.png"
+						alt=""
+						className="h-[80px] w-auto flex-shrink-0"
 					/>
 				</div>
-			</main>
+				<img
+					src="whitetext.png"
+					alt="hack404 big label"
+					className="h-auto w-full"
+				/>
+			</div>
 
+			{/* Horizontal border */}
+			<HBorder />
+
+			{/* Gradient border */}
 			<GradientBorder reverse={true} />
+
+			{/* Footer */}
 			<footer className="homepage-footer">
 				<h1 className="text-2xl w-[300px] mr-40 text-white font-(family-name:--font-heading)">
 					Coming soon <br /> July 2025
@@ -47,15 +48,12 @@ export default function Home() {
 							enter your email
 						</span>
 					</div>
-					<input
-						type="text"
-						className="text-2xl flex-grow text-white font-(family-name:--font-heading-light) focus:border-transparent focus:outline-none"
-					/>
 				</div>
 
+				{/* Submit button */}
 				<div className="relative flex items-center justify-center w-[477px] h-[78px]">
 					<svg
-						className="absolute inset-0 z-0"
+						className="absolute inset-0 -z-10"
 						xmlns="http://www.w3.org/2000/svg"
 						width="282"
 						height="78"
@@ -67,13 +65,17 @@ export default function Home() {
 							fill="white"
 						/>
 					</svg>
-
-					<div className="relative z-10 flex items-center justify-between gap-4 text-2xl text-black w-full mx-[24px] font-(family-name:--font-heading)">
+					<div className="relative flex items-center justify-between gap-4 text-2xl text-black w-full mx-[24px] font-(family-name:--font-heading)">
 						<p>Submit</p>
 						<IoMdArrowForward size={24} />
 					</div>
 				</div>
 			</footer>
+
+			<div className="-z-20">
+				<Grid />
+			</div>
+			<GradientBackground />
 		</div>
 	);
 }

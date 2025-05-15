e543397d893cafbfba9dd8d8c6cd292db2877f7c
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-05-15 00:33:05 -0400

    added osme touches

--- src/app/components/email-template.tsx
+++ src/app/components/email-template.tsx
@@ -5,9 +5,21 @@ interface EmailTemplateProps {
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({}) => (
-  <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px', color: '#333' }}>
+  <div style={{ 
+    fontFamily: "'FH Lecturis', Arial, sans-serif", 
+    padding: '20px', 
+    color: '#333' 
+  }}>
     <p>Hey there, thank you for signing up for Hack404's information list! We're excited for our first iteration happening this July 4-6. Stay tuned - hacker applications will open shortly.</p>
     <p>In the meantime, check out our Instagram - <a href="https://www.instagram.com/hack404.dev/" style={{ color: '#0366d6', textDecoration: 'none' }}>@hack404.dev</a>.</p>
     <p>Sincerely,<br />The Hack404 Team</p>
+    
+    <div style={{ marginTop: '20px', textAlign: 'left' }}>
+      <img 
+        src="https://hack404.dev/blackfull.png" 
+        alt="Hack404 Logo" 
+        style={{ maxWidth: '150px', height: 'auto' }}
+      />
+    </div>
   </div>
 );

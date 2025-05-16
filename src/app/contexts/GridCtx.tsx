ddf3c13389e64cbb0656121f2b569f4c8706f196
gooosexe <olivershaohuang2006@gmail.com>
2025-05-16 02:17:32 -0400

    formatted gridctx

--- src/app/contexts/GridCtx.tsx
+++ src/app/contexts/GridCtx.tsx
@@ -1,16 +1,22 @@
 import { createContext, useContext, useState } from "react";
 
-  const GridColWidthCtx = createContext([0, (width: number) => {}] as [number, (width: number) => void]);
+const GridColWidthCtx = createContext([0, (width: number) => {}] as [
+  number,
+  (width: number) => void,
+]);
 
+export const GridColWidthProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [colWidth, setColWidth] = useState(0);
 
-  export const GridColWidthProvider = ({ children }: { children: React.ReactNode }) => {
-    const [colWidth, setColWidth] = useState(0);
-
-    return (
-      <GridColWidthCtx.Provider value={[colWidth, setColWidth]}>
-        {children}
-      </GridColWidthCtx.Provider>
-    );
-  }
+  return (
+    <GridColWidthCtx.Provider value={[colWidth, setColWidth]}>
+      {children}
+    </GridColWidthCtx.Provider>
+  );
+};
 
 export const useGridColWidth = () => useContext(GridColWidthCtx);

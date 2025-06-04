506e0d01e27bbf2e92df883ed0aec1951dbce126
Andrew Chu <andrew.chu206@gmail.com>
2025-06-04 02:11:17 -0400

    Create pick stream page

---
+++ src/app/(protected)/application/beginner/page.tsx
@@ -0,0 +1,3 @@
+export default function BeginnerApplicationPage() {
+  return <div>BeginnerApplicationPage</div>;
+}
---
+++ src/app/(protected)/application/normal/page.tsx
@@ -0,0 +1,3 @@
+export default function NormalApplicationPage() {
+  return <div>NormalApplicationPage</div>;
+}

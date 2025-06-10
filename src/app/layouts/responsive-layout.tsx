997e92d53ab0b94b917315bd11d632b9f064aea5
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2025-06-09 22:19:03 -0400

    feat: update event details and add FAQ section for Hack404

--- src/app/layouts/responsive-layout.tsx
+++ src/app/layouts/responsive-layout.tsx
@@ -175,14 +175,88 @@ export default function ResponsiveLayout({ handleSubmit }: LayoutProps) {
               hack404?
             </TextSectionTitle>
             <TextSectionContent enableGradientWords={true}>
-              A 36-hour hackathon based in Toronto where secondary and
-              post-secondary students, from first-time hackers to seasoned
-              builders, team up to solve real-world problems and shape
-              tomorrow's technology.
+              {"Hack404 is a 36-hour hackathon in Toronto, ON. \
+              We're hoping to bring together 200 students who are \
+              passionate about learning and building to create projects that excite them. \
+              We're focused on our three pillars: education, innovation, and community."}
             </TextSectionContent>
           </TextSection>
 
           <HBorder />
+
+          <TextSection titleWidth={1} contentWidth={3} className="">
+            <TextSectionTitle>
+              Event Structure
+            </TextSectionTitle>
+          </TextSection>
+
+          <TextSection titleWidth={1} contentWidth={3} className="py-4 tablet:py-4 desktop:py-4">
+            <TextSectionTitle className="font-(family-name:--font-heading-light)">
+              <GradientBorder>Beginner stream</GradientBorder>
+            </TextSectionTitle>
+            <TextSectionContent enableGradientWords={true}>
+              {"Beginner teams will attend 3-5 workshops throughout the weekend, creating ‘mini-hacks’ rather than a single large project. Then, they’ll be judged portfolio-style, where they showcase their top 3 mini-hacks and are evaluated based on their technical execution, creativity, and innovation."}
+            </TextSectionContent>
+          </TextSection>
+          <TextSection titleWidth={1} contentWidth={3} className="">
+            <TextSectionTitle className="font-(family-name:--font-heading-light)">
+              <GradientBorder>Regular stream</GradientBorder>
+            </TextSectionTitle>
+            <TextSectionContent enableGradientWords={true}>
+              {"Hackers will have 36 hours to create an innovative project in line with our three pillars—education, innovation, and community—and a problem statement that will be announced during opening ceremonies. We’ll also be hosting activities and workshops throughout the weekend!"}
+            </TextSectionContent>
+          </TextSection>
+
+          <HBorder />
+
+          <TextSection titleWidth={1} contentWidth={3}>
+            <TextSectionTitle>
+              Who can participate?
+            </TextSectionTitle>
+            <TextSectionContent enableGradientWords={true}>
+              {"Hack404 is for students, by students. Whether you’re a beginner hacker or have been to 10+ hackathons, we’d love for you to continue to innovate at Hack404. We welcome applications from high school and post-secondary students, as well as those who graduated in 2025."}
+            </TextSectionContent>
+          </TextSection>
+
+          <HBorder />
+            
+          <TextSection titleWidth={1} contentWidth={3}>
+            <TextSectionTitle>
+              FAQ
+            </TextSectionTitle>
+          </TextSection>
+
+          <TextSection titleWidth={1} contentWidth={3} className="">
+            <TextSectionTitle className="font-(family-name:--font-heading-light)">
+              <GradientBorder>How do I register?</GradientBorder>
+            </TextSectionTitle>
+            <TextSectionContent enableGradientWords={true}>
+              {"Applications are open now! Go to hack404.dev and click ‘Sign up now’!"}
+            </TextSectionContent>
+          </TextSection>
+          
+          <TextSection titleWidth={1} contentWidth={3} className="">
+            <TextSectionTitle className="font-(family-name:--font-heading-light)">
+              <GradientBorder>How do I register?</GradientBorder>
+            </TextSectionTitle>
+            <TextSectionContent enableGradientWords={true}>
+              {"No! We’re open to beginners and experienced hackers, and we’re especially looking for students who are just starting out in the field of tech for our beginner stream."}
+            </TextSectionContent>
+          </TextSection>
+
+          <TextSection titleWidth={1} contentWidth={3} className="">
+            <TextSectionTitle className="font-(family-name:--font-heading-light)">
+              <GradientBorder>Do I need an idea before the hackathon?</GradientBorder>
+            </TextSectionTitle>
+            <TextSectionContent enableGradientWords={true}>
+              {"No! You’ll have time and support during the hackathon to create and refine your ideas. Feel free to start brainstorming now, but keep in mind that our problem statements will only be announced at our opening ceremonies."}
+            </TextSectionContent>
+          </TextSection>
+
+          <HBorder />
+
+  
+
 
           <TextSection titleWidth={1} contentWidth={3} className="pb-2 tablet:pb-2 desktop:pb-2">
             <TextSectionTitle>
@@ -192,6 +266,7 @@ export default function ResponsiveLayout({ handleSubmit }: LayoutProps) {
               Link to our socials
             </TextSectionContent>
           </TextSection>
+
 
           <TextSection titleWidth={1} contentWidth={3} className="pt-2 tablet:pt-2 desktop:pt-2">
             <TextSectionTitle>

1540b5e368e707a329c253815c80ac201b83fdec
osh3276 <osh3276@gmail.com>
2025-06-09 23:32:43 -0400

    emergency removal of faq text

--- src/app/layouts/responsive-layout.tsx
+++ src/app/layouts/responsive-layout.tsx
@@ -8,7 +8,11 @@ import GradientBackground from "@/components/gradient-background";
 import WaitlistBox from "../../components/waitlist-box";
 import CutButton from "@/components/cut-button";
 import { useGridColWidth } from "../contexts/GridCtx";
-import { TextSection, TextSectionTitle, TextSectionContent } from "@/components/text-section";
+import {
+  TextSection,
+  TextSectionTitle,
+  TextSectionContent,
+} from "@/components/text-section";
 import Image from "next/image";
 import { InstagramButton } from "@/components/social-button";
 
@@ -151,7 +155,7 @@ export default function ResponsiveLayout({ handleSubmit }: LayoutProps) {
             <img
               src="https://images.unsplash.com/photo-1698957921407-bf292bcacf5e"
               alt="Toronto skyline at night"
-              className="rounded-lg" 
+              className="rounded-lg"
             />
           </div>
         </div>
@@ -167,7 +171,9 @@ export default function ResponsiveLayout({ handleSubmit }: LayoutProps) {
         id="about-us"
         className=" relative flex min-h-screen flex-col justify-between"
       >
-        <div className="flex flex-col items-start justify-between text-left text-white">          {/* about us */}
+        <div className="flex flex-col items-start justify-between text-left text-white">
+          {" "}
+          {/* about us */}
           <div className="bg-background absolute inset-0 -z-30" />
           <TextSection titleWidth={1} contentWidth={3}>
             <TextSectionTitle>
@@ -175,119 +181,14 @@ export default function ResponsiveLayout({ handleSubmit }: LayoutProps) {
               hack404?
             </TextSectionTitle>
             <TextSectionContent enableGradientWords={true}>
-              {"Hack404 is a 36-hour hackathon in Toronto, ON. \
+              {
+                "Hack404 is a 36-hour hackathon in Toronto, ON. \
               We're hoping to bring together 200 students who are \
               passionate about learning and building to create projects that excite them. \
-              We're focused on our three pillars: education, innovation, and community."}
+              We're focused on our three pillars: education, innovation, and community."
+              }
             </TextSectionContent>
           </TextSection>
-
-          <HBorder />
-
-          <TextSection titleWidth={1} contentWidth={3} className="">
-            <TextSectionTitle>
-              Event Structure
-            </TextSectionTitle>
-          </TextSection>
-
-          <TextSection titleWidth={1} contentWidth={3} className="py-4 tablet:py-4 desktop:py-4">
-            <TextSectionTitle className="font-(family-name:--font-heading-light)">
-              <GradientBorder>Beginner stream</GradientBorder>
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true}>
-              {"Beginner teams will attend 3-5 workshops throughout the weekend, creating ‘mini-hacks’ rather than a single large project. Then, they’ll be judged portfolio-style, where they showcase their top 3 mini-hacks and are evaluated based on their technical execution, creativity, and innovation."}
-            </TextSectionContent>
-          </TextSection>
-          <TextSection titleWidth={1} contentWidth={3} className="">
-            <TextSectionTitle className="font-(family-name:--font-heading-light)">
-              <GradientBorder>Regular stream</GradientBorder>
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true}>
-              {"Hackers will have 36 hours to create an innovative project in line with our three pillars—education, innovation, and community—and a problem statement that will be announced during opening ceremonies. We’ll also be hosting activities and workshops throughout the weekend!"}
-            </TextSectionContent>
-          </TextSection>
-
-          <HBorder />
-
-          <TextSection titleWidth={1} contentWidth={3}>
-            <TextSectionTitle>
-              Who can participate?
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true}>
-              {"Hack404 is for students, by students. Whether you’re a beginner hacker or have been to 10+ hackathons, we’d love for you to continue to innovate at Hack404. We welcome applications from high school and post-secondary students, as well as those who graduated in 2025."}
-            </TextSectionContent>
-          </TextSection>
-
-          <HBorder />
-            
-          <TextSection titleWidth={1} contentWidth={3}>
-            <TextSectionTitle>
-              FAQ
-            </TextSectionTitle>
-          </TextSection>
-
-          <TextSection titleWidth={1} contentWidth={3} className="">
-            <TextSectionTitle className="font-(family-name:--font-heading-light)">
-              <GradientBorder>How do I register?</GradientBorder>
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true}>
-              {"Applications are open now! Go to hack404.dev and click ‘Sign up now’!"}
-            </TextSectionContent>
-          </TextSection>
-          
-          <TextSection titleWidth={1} contentWidth={3} className="">
-            <TextSectionTitle className="font-(family-name:--font-heading-light)">
-              <GradientBorder>How do I register?</GradientBorder>
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true}>
-              {"No! We’re open to beginners and experienced hackers, and we’re especially looking for students who are just starting out in the field of tech for our beginner stream."}
-            </TextSectionContent>
-          </TextSection>
-
-          <TextSection titleWidth={1} contentWidth={3} className="">
-            <TextSectionTitle className="font-(family-name:--font-heading-light)">
-              <GradientBorder>Do I need an idea before the hackathon?</GradientBorder>
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true}>
-              {"No! You’ll have time and support during the hackathon to create and refine your ideas. Feel free to start brainstorming now, but keep in mind that our problem statements will only be announced at our opening ceremonies."}
-            </TextSectionContent>
-          </TextSection>
-
-          <HBorder />
-
-  
-
-
-          <TextSection titleWidth={1} contentWidth={3} className="pb-2 tablet:pb-2 desktop:pb-2">
-            <TextSectionTitle>
-              Contact
-            </TextSectionTitle>
-            <TextSectionContent>
-              Link to our socials
-            </TextSectionContent>
-          </TextSection>
-
-
-          <TextSection titleWidth={1} contentWidth={3} className="pt-2 tablet:pt-2 desktop:pt-2">
-            <TextSectionTitle>
-              <span className="text-2xl font-normal">
-                <GradientBorder>
-                    emma.xing@power-unit.org
-                </GradientBorder>
-              </span>
-            </TextSectionTitle>
-            <TextSectionContent enableGradientWords={true} as="div">
-              <div className="flex flex-col gap-4">
-                <div className="flex gap-3">
-                  <InstagramButton 
-                    url="https://www.instagram.com/hack404.dev/"
-                    size="md"
-                  />
-                </div>
-              </div>
-            </TextSectionContent>
-          </TextSection>
-          <HBorder />
         </div>
       </div>
     </>

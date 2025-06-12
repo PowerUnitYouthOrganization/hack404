7d6ba3330edad54b4b8f3f99873f06973e5d587d
osh3276 <osh3276@gmail.com>
2025-06-12 12:21:19 -0400

    Remove unused query.ts debug script

--- src/db/query.ts
+++
@@ -1,9 +0,0 @@
-import { waitlistDb, waitlistEmails } from "./waitlistSchema";
-
-async function viewTable() {
-  // Fetch all emails from the waitlist
-  const emails = await waitlistDb.select().from(waitlistEmails);
-  console.log("Table contents:", emails);
-}
-
-void viewTable();

f40e5ffc661a98662871d66e5fca0877b1307fe7
osh3276 <osh3276@gmail.com>
2025-06-30 18:37:00 -0400

    feat: Update calendar grid and event details

--- src/app/(protected)/launchpad/containers/calendar-grid.tsx
+++ src/app/(protected)/launchpad/containers/calendar-grid.tsx
@@ -116,7 +116,9 @@ export default function CalendarGrid({
       {/* Fixed Header */}
       <div className="flex px-6 py-6 justify-center items-center gap-2.5 self-stretch bg-inherit backdrop-blur-[25px] border-b border-[rgba(48,242,242,0.2)] sticky top-0 z-10 flex-shrink-0">
         <h1 className="flex-1 text-white font-light">{title}</h1>
-        <p className="font-light text-wcyan"></p>
+        <p className="font-light text-wcyan">
+          Hacking lasts from Friday 9:00PM to Sunday 9:00AM
+        </p>
         {icon && <div className="text-white h-5 w-5">{icon}</div>}
       </div>
 

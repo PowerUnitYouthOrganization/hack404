5fde92a69a181fc9e56999e3eb16da75362d3dcc
osh3276 <osh3276@gmail.com>
2025-07-02 22:07:18 -0400

    Update header navigation links and add logos

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ export default function Header() {
     <header>
       <nav className="flex h-[64px] w-full flex-shrink-0 items-start gap-[24px] px-[64px]">
         <a
-          href="/launchpad"
+          href="#about-us"
           className="flex flex-1 flex-col items-start justify-center gap-[10px] self-stretch text-white"
         >
-          Agenda
+          About Us
         </a>
         <a
-          href="/sponsors"
+          href="/sponsors/"
           target="_blank"
           rel="noopener noreferrer"
           className="flex flex-1 flex-col items-start justify-center gap-[10px] self-stretch text-white"
@@ -19,22 +19,66 @@ export default function Header() {
           Sponsors
         </a>
         <a
-          href="#faq"
-          className="flex flex-1 flex-col items-start justify-center gap-[10px] self-stretch text-white"
+          href=""
+          className="flex flex-1 flex-col items-center justify-center gap-[10px] self-stretch"
         >
-          FAQ
+          <img
+            src="whitefull.png"
+            alt="Logo"
+            className="flex h-auto w-[109px] items-center justify-center"
+          />
         </a>
         <a
-          href=""
-          className="flex flex-1 flex-col items-end justify-center gap-[10px] self-stretch"
-        ></a>
+          href="/Hack404 Sponsorship Package.pdf"
+          className="flex flex-1 flex-col items-end justify-center gap-[10px] self-stretch text-white"
+        >
+          Sponsor us
+        </a>
         <a
-          href="/login"
+          href="https://power-unit.org"
           className="flex flex-1 flex-col items-end justify-center gap-[10px] self-stretch"
         >
-          Log In
+          <img
+            src="PUYOlogo.png"
+            alt="Logo"
+            className="flex h-auto w-[49px] items-center justify-center"
+          />
         </a>
       </nav>
     </header>
+    // <header>
+    //   <nav className="flex h-[64px] w-full flex-shrink-0 items-start gap-[24px] px-[64px]">
+    //     <a
+    //       href="/launchpad"
+    //       className="flex flex-1 flex-col items-start justify-center gap-[10px] self-stretch text-white"
+    //     >
+    //       Agenda
+    //     </a>
+    //     <a
+    //       href="/sponsors"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //       className="flex flex-1 flex-col items-start justify-center gap-[10px] self-stretch text-white"
+    //     >
+    //       Sponsors
+    //     </a>
+    //     <a
+    //       href="#faq"
+    //       className="flex flex-1 flex-col items-start justify-center gap-[10px] self-stretch text-white"
+    //     >
+    //       FAQ
+    //     </a>
+    //     <a
+    //       href=""
+    //       className="flex flex-1 flex-col items-end justify-center gap-[10px] self-stretch"
+    //     ></a>
+    //     <a
+    //       href="/login"
+    //       className="flex flex-1 flex-col items-end justify-center gap-[10px] self-stretch"
+    //     >
+    //       Log In
+    //     </a>
+    //   </nav>
+    // </header>
   );
 }
